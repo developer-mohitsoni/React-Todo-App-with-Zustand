4fe89484ceb0b9530b4eb9abdeb8680b52c7be28
Mohit Soni <developer_mohitsoni@outlook.com>
2024-10-13 21:23:47 +0530

    Complete React Todo-App using Zustand

---
+++ zustand_todo/src/store/todoStore.ts
@@ -0,0 +1,46 @@
+import { create } from "zustand";
+
+import { devtools, persist } from "zustand/middleware";
+
+export interface TodoType {
+  id: number;
+  todo: string;
+  isDone: boolean;
+}
+interface States {
+  todos: Array<TodoType> | [];
+}
+
+interface Actions {
+  addTodo: (todo: TodoType) => void;
+  toggleTodo: (id: number, isChecked: boolean) => void;
+  deleteTodo: (id: number) => void;
+}
+
+export const todoStore = create<States & Actions>()(
+  devtools(
+    persist(
+      (set) => ({
+        todos: [],
+
+        addTodo: (todo: TodoType) =>
+          set((state) => ({ todos: [todo, ...state.todos] })),
+
+        toggleTodo: (id: number, isChecked: boolean) =>
+          set((state) => ({
+            todos: state.todos.map((item) => {
+              if (item.id === id) {
+                item.isDone = isChecked;
+              }
+              return item;
+            }),
+          })),
+        deleteTodo: (id: number) =>
+          set((state) => ({
+            todos: state.todos.filter((item) => item.id !== id),
+          })),
+      }),
+      { name: "todoStore" }
+    )
+  )
+);
