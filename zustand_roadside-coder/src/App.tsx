e3261abad4dc2026f7f5cc370c5d7946287f13a3
Mohit Soni <developer_mohitsoni@outlook.com>
2024-10-14 18:16:25 +0530

    Basic Folder Structure Setup

---
+++ zustand_roadside-coder/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@mui/material";
+
+const App = () => {
+  return (
+    <>
+      <div>App</div>
+      <Button variant="contained">Contained</Button>
+    </>
+  );
+};
+
+export default App;
