404c30636121eef94d624a87027bf1fe58423f99
Mohit Soni <developer_mohitsoni@outlook.com>
2024-10-15 11:24:37 +0530

    Basic Setup of Project using Shadcn

---
+++ zustand_by_Code-Genix/src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "./components/ui/button";
+
+const App = () => {
+  return (
+    <>
+      <div>
+        <Button>Hello World</Button>
+      </div>
+    </>
+  );
+};
+
+export default App;
