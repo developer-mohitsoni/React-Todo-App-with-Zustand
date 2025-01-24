e2f927c77c5967b1c566b01887ef5e1d94fe5d84
Mohit Soni <developer_mohitsoni@outlook.com>
2025-01-24 17:07:50 +0530

    Refactor App component to use Zustand for state management

--- zustand_by_Death-Code/src/App.tsx
+++ zustand_by_Death-Code/src/App.tsx
@@ -1,10 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { useCount } from "./store/countStore";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { count, incCount } = useCount();
+
+  console.log(count);
+  console.log(incCount);
 
   return (
     <>
@@ -18,9 +21,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <button onClick={() => incCount(7)}>count is {count}</button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
@@ -29,7 +30,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
