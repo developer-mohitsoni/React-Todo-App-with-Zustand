87253d096f2bfd7ebccbb7e227a71916bc6b96fb
Mohit Soni <developer_mohitsoni@outlook.com>
2025-01-24 17:30:28 +0530

    Enhance Zustand store with persistence and devtools middleware

--- zustand_by_Death-Code/src/store/countStore.ts
+++ zustand_by_Death-Code/src/store/countStore.ts
@@ -1,22 +1,30 @@
 import { create } from "zustand";
+import { persist, devtools } from "zustand/middleware";
 
 interface Count {
   count: number;
   incCount: (num: number) => void;
   decCount: (num: number) => void;
 }
-export const useCount = create<Count>((set) => ({
-  count: 0,
-  incCount: (num) => {
-    // console.log("Clicked");
-    // const promise = new Promise((resolve) => {
-    //   setTimeout(resolve, 2000);
-    // });
-    // await promise;
-    set((state) => ({ ...state, count: state.count + num }));
-  },
-  decCount: (num) => {
-    //  Ensure count does not go below 0
-    set((state) => ({ ...state, count: Math.max(0, state.count - num) }));
-  },
-}));
+export const useCount = create<Count>()(
+  devtools(
+    persist(
+      (set) => ({
+        count: 0,
+        incCount: (num) => {
+          // console.log("Clicked");
+          // const promise = new Promise((resolve) => {
+          //   setTimeout(resolve, 2000);
+          // });
+          // await promise;
+          set((state) => ({ ...state, count: state.count + num }));
+        },
+        decCount: (num) => {
+          //  Ensure count does not go below 0
+          set((state) => ({ ...state, count: Math.max(0, state.count - num) }));
+        },
+      }),
+      { name: "countPanda" }
+    )
+  )
+);
